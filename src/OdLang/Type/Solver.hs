7b649e12a39045cd006316e6dcad4c43fb270929
Nikita Solodovnikov <nas140301@gmail.com>
2025-05-17 02:10:54 +0300

    Separate metavariables in solver

--- src/OdLang/Type/Solver.hs
+++ src/OdLang/Type/Solver.hs
@@ -8,6 +8,7 @@ import Control.Monad.State
 import Data.Bifunctor
 import Data.Bifoldable
 import Data.Bitraversable
+import Data.Bifunctor
 import Data.Bifunctor.TH
 import Data.Data
 import Data.Function
@@ -95,11 +96,17 @@ pattern TUnit = Free TUnitF
 
 infixr 6 :->
 
+newtype Skolem = Skolem Int deriving (Eq, Show, Typeable, Data)
+
+type SkInc var = Either Skolem (Inc var)
+
+type Skope term var = term (SkInc var)
+
 data Constraint f var where
   CEq :: Free f var -> Free f var -> Constraint f var
   CPred :: Pred -> [Free f var] -> Constraint f var
-  CImpl :: [Scope (Constraint f) var]
-        -> Scope (Constraint f) var
+  CImpl :: [Constraint f (Either Skolem var)]
+        -> [Skope (Constraint f) var]
         -> Constraint f var
 
 deriving instance (Show var, Show2 f) => Show (Constraint f var)
@@ -111,20 +118,27 @@ substitute :: Bifunctor f => (var -> Free f var') -> Constraint f var -> Constra
 substitute s (CEq t1 t2) = (t1 >>= s) `CEq` (t2 >>= s)
 substitute s (CPred p t) = CPred p $ map (>>= s) t
 substitute s (CImpl a c) =
-  CImpl (map (substitute (traverse s)) a) (substitute (traverse s) c)
+  CImpl (map (substitute (traverse s)) a)
+        (map (substitute (traverse $ traverse s)) c)
 
-weaken :: Functor f => f var -> Scope f var
-weaken = fmap F
+weaken :: Functor f => f var -> Skope f var
+weaken = fmap (Right . F)
 
-contract :: Traversable f => Scope f var -> Maybe (f var)
-contract = traverse \case
+contract :: Traversable f => Skope f var -> Maybe (f var)
+contract = sequence . fmap \case
+  Left _ -> Nothing
+  Right (B _) -> Nothing
+  Right (F v) -> Just v
+
+contractT :: Traversable f => Scope f var -> Maybe (f var)
+contractT = sequence . fmap \case
   B _ -> Nothing
   F v -> Just v
 
 sameConstr :: Data a => a -> a -> Bool
 sameConstr = (==) `on` toConstr
 
-unroll :: (Bifoldable f)
+unroll :: Bifoldable f
        => FreeS f var
        -> [Either (Scope (Free f) var) (Free f var)]
 unroll = bifoldr (\s l -> Left s : l) (\f l -> Right f : l) []
@@ -135,26 +149,30 @@ matches :: forall f var .
         , Eq2 f
         , Bifunctor f, Bifoldable f, Bitraversable f
         , Eq var, Data var)
-        => [Free f var] -> [Scope (Free f) var]
-        -> Maybe (Inc var -> Free f var)
+        => [Free f var] -> [Free f (Either Skolem var)]
+        -> Maybe (Either Skolem var -> Free f var)
 matches a b = fmap sub $ join $ fmap sequence $ combineList $ zipWith (matches' lvl) a b
   where
-    matches' :: (Eq var', Data var') => (Inc var' -> Maybe Int) -> Free f var' -> Scope (Free f) var'
+    matches' :: (Eq var', Data var', Data var'') => (var'' -> Either Int var') -> Free f var' -> Free f var''
              -> Maybe (M.IntMap (Maybe (Free f var')))
-    matches' lvl t (Pure (lvl -> Just i)) = Just $ M.singleton i (Just t)
+    matches' lvl t (Pure (lvl -> Left i)) = Just $ M.singleton i (Just t)
+    matches' lvl (Pure v1) (Pure (lvl -> Right v2))
+      | v1 == v2 = Just M.empty
+      | otherwise = Nothing
     matches' lvl (Free t1) (Free t2)
       | toConstr t1 == toConstr t2 =
         let (s1, f1) = partitionEithers $ unroll t1
             (s2, f2) = partitionEithers $ unroll t2
-            lvl' (F v) = lvl v
-            lvl' _ = Nothing
-         in combineList $ (fmap (fmap (>>= contract)) <$> zipWith (matches' lvl') s1 s2) ++ zipWith (matches' lvl) f1 f2
+            lvl' (F v) = second F $ lvl v
+            lvl' (B i) = Right $ B i
+            terms = zipWith (matches' lvl) f1 f2
+            scopeds = zipWith (matches' lvl') s1 s2
+         in combineList $ (fmap (fmap (>>= contractT)) <$> scopeds) ++ terms
       | otherwise = Nothing
     matches' _ _ _ = Nothing
 
-    lvl :: Inc var -> Maybe Int
-    lvl (B i) = Just i
-    lvl _ = Nothing
+    lvl :: Either Skolem var -> Either Int var
+    lvl = first \case {Skolem i -> i}
 
     combine :: Eq var' => M.IntMap (Maybe (Free f var')) -> M.IntMap (Maybe (Free f var'))
             -> M.IntMap (Maybe (Free f var'))
@@ -165,8 +183,20 @@ matches a b = fmap sub $ join $ fmap sequence $ combineList $ zipWith (matches' 
     combineList = foldl' (liftM2 combine) (Just M.empty)
 
     sub m = \case
-      B i -> m M.! i
-      F v -> pure v
+      Left (Skolem i) -> m M.! i
+      Right v -> pure v
+
+ensureAssumption :: Bitraversable f => Skope (Constraint f) var -> Maybe (Constraint f (Either Skolem var))
+ensureAssumption = sequence . fmap \case
+                     Left sk -> Just $ Left sk
+                     Right (F v) -> Just $ Right v
+                     Right (B i) -> Nothing
+
+fromAssumption :: Bifunctor f => Constraint f (Either Skolem var) -> Skope (Constraint f) var
+fromAssumption = fmap (fmap F)
+
+withSkolems :: Bifunctor f => Scope (Free f) var -> Skope (Free f) var
+withSkolems = fmap Right
 
 solve :: forall f var .
       ( forall v . Data v => Data (FreeS f v)
@@ -183,7 +213,7 @@ solve assume consts = evalState (go assume consts []) False
 
     genConstraints lhs rhs = zipWith constr (unroll lhs) (unroll rhs)
 
-    constr (Left t1) (Left t2) = CImpl [] (CEq t1 t2)
+    constr (Left t1) (Left t2) = CImpl [] [CEq (withSkolems t1) (withSkolems t2)]
     constr (Right t1) (Right t2) = CEq t1 t2
 
     go :: [Constraint f var] -> [Constraint f var] -> [Constraint f var]
@@ -198,7 +228,7 @@ solve assume consts = evalState (go assume consts []) False
           pure cs
 
     go assume (c:cs) r =
-      trace ("assumptions = " ++ show assume ++ "\nconstraints = " ++ show (reverse r ++ (c:cs))) $
+      trace ("assumptions = " ++ show assume ++ "\nconstraints = " ++ show (reverse r ++ (c:cs)) ++ "\n") $
       case c of
         CEq t1 t2 | t1 == t2 -> put True >> go assume cs r
         CEq (Pure a) t -> put True >> go assume (map (substitute (subst a t)) cs) (map (substitute (subst a t)) r)
@@ -214,12 +244,13 @@ solve assume consts = evalState (go assume consts []) False
             _:_  -> error "Ambiguous assumptions"
             [] ->
               case [map (substitute m) a
-                   | a `CImpl` (CPred p' ts') <- assume
+                   | a `CImpl` [ensureAssumption -> Just (CPred p' ts')] <- assume
                    , m <- maybeToList $ matches ts ts'
                    , p == p', length ts == length ts'] of
                 [a] -> put True >> go assume (a ++ cs) r
                 _:_ -> error "Ambiguous assumptions"
                 [] -> go assume cs (c:r)
 
-        CImpl a c -> let Just res = traverse contract $ solve (map weaken assume ++ a) [c]
-                      in put True >> go assume (res ++ cs) r
+        CImpl (map fromAssumption -> a) c
+          -> let Just res = traverse contract $ solve (map weaken assume ++ a) c
+              in put True >> go assume (res ++ cs) r
